dce5dc2c8361d530780dc437b5a0e330dafa92ff
Timothy Kimathi <timsnky@gmail.com>
2022-01-05 13:12:42 +0300

    Updated home and about page to use layout template

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 1',
+        'content': 'First log post',
+        'date_posted': 'January 5, 2022'
+    },
+    {
+        'author': 'Joe Public',
+        'title': 'Blog Post 2',
+        'content': 'Second log post',
+        'date_posted': 'January 6, 2022'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
